7418420c3fc31128f4b5f605062500eabc612de0
Masixole <masixole.menqe@umuzi.org>
2019-09-21 14:32:05 +0200

    made the code to be in a form of a class

--- spec/CalculatorSpec.js
+++ spec/CalculatorSpec.js
@@ -1,48 +1,53 @@
 
 describe("A simple calculator that can add 2 numbers", function(){
-   
+        let test = new SimpleCalculator()
     it ("should add 0 and 0 to return 0", function() {
         
-        expect(add(0,0)).toEqual(0);
+        expect(test.add(0,0)).toEqual(0);
 
     });
 
     it ("should return the sum of -1 and -1 as -2", function() {
         
-        expect(add(-1,-1)).toEqual(-2);
+        expect(test.add(-1,-1)).toEqual(-2);
     });
 
     it ("should return 9 as the result of adding 4 and 5", function(){
       
-        expect(add(4,5)).toEqual(9);
+        expect(test.add(4,5)).toEqual(9);
     });
 });
 
 describe("A simple calculator that can add as many numbers as it is given", function(){
     
+    let test = new SimpleCalculator();
+    
     it ("should then be able to perfectly add all the numbers in the array", function() {
         
-        expect(add(1,2,3,4,5)).toEqual(15);
+        expect(test.add(1,2,3,4,5)).toEqual(15);
     });
 });
 
 describe("Checking simple calculator that multiply 2 numbers", function() {
-  
+    
+    let test = new SimpleCalculator();
+    
     it ("should be able to multiply any two negative numbers", function() {
       
-        expect(multiply(-1,-2)).toEqual(2);
+        expect(test.multiply(-1,-2)).toEqual(2);
     });
 
     it ("should be able to multiply any two positive numbers", function() {
     
-        expect(multiply(5,6)).toEqual(30);
+        expect(test.multiply(5,6)).toEqual(30);
     });
 });
 
 describe ("Simple calculator that can multiply multiple given numbers", function() {
+    let test = new SimpleCalculator();
        
         it ("should be able to multiply any amount of given numbers, negative and positive", function() {
              
-            expect(multiply(5,5,-1,2,2)).toEqual(-100);
+            expect(test.multiply(5,5,-1,2,2)).toEqual(-100);
         });
-})
+});
--- src/CalculatorSrc.js
+++ src/CalculatorSrc.js
@@ -1,19 +1,49 @@
- function add() {
-  var sum = 0;
-  for (i = 0; i < arguments.length; i++) {
-    sum = sum + arguments[i]
-  };
-   return sum;
-};
- 
-function multiply(){
+ class SimpleCalculator {
+   add(){
+      var sum = 0;
+        for (let i = 0; i < arguments.length; i++) {
+          sum = sum + arguments[i];
+        }
+         return sum;
+   }
+
+   multiply(){
     var result = 1;
-    for (i = 0; i < arguments.length; i++){
-    result = result * arguments[i];
-};
- return result;
+        for (let i = 0; i < arguments.length; i++){
+        result = result * arguments[i];
+    };
+     return result;
+   }
+  
+  }
 
-}
 
-console.log(add(1,2,3,4));
-console.log(multiply(1,2,3,4));
+
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+//   function add() {
+//   var sum = 0;
+//   for (i = 0; i < arguments.length; i++) {
+//     sum = sum + arguments[i]
+//   };
+//    return sum;
+// };
+ 
+// function multiply(){
+//     var result = 1;
+//     for (i = 0; i < arguments.length; i++){
+//     result = result * arguments[i];
+// };
+//  return result;
+
+// };
+
+// console.log(add(1,2,3,4));
+// console.log(multiply(1,2,3,4));
