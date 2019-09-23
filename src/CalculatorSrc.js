ba41e22b478754d8c46ff4c042a8bc60a9781499
Masixole Menqe <masixole.menqe@umuzi.org>
2019-09-23 15:24:26 +0200

    cleaned the code

--- src/CalculatorSrc.js
+++ src/CalculatorSrc.js
@@ -15,35 +15,6 @@
      return result;
    }
   
-  }
-
-
-
- 
- 
- 
- 
- 
- 
- 
- 
- 
-//   function add() {
-//   var sum = 0;
-//   for (i = 0; i < arguments.length; i++) {
-//     sum = sum + arguments[i]
-//   };
-//    return sum;
-// };
- 
-// function multiply(){
-//     var result = 1;
-//     for (i = 0; i < arguments.length; i++){
-//     result = result * arguments[i];
-// };
-//  return result;
-
-// };
 
 // console.log(add(1,2,3,4));
 // console.log(multiply(1,2,3,4));
