8b57e22f654eca8f3fcf137a91d5893b9ae885a5
Masixole Menqe <masixole.menqe@umuzi.org>
2019-11-01 09:25:40 +0200

    changes made after code revision

--- src/CalculatorSrc.js
+++ src/CalculatorSrc.js
@@ -1,20 +1,22 @@
- class SimpleCalculator {
-   add(){
-      var sum = 0;
-        for (let i = 0; i < arguments.length; i++) {
-          sum = sum + arguments[i];
-        }
-         return sum;
-   }
+class SimpleCalculator {
+  add(){
+     var sum = 0;
+       for (let i = 0; i < arguments.length; i++) {
+         sum = sum + arguments[i];
+       }
+        return sum;
+  }
 
-   multiply(){
-    var result = 1;
-        for (let i = 0; i < arguments.length; i++){
-        result = result * arguments[i];
-    };
-     return result;
-   }
-  
+  multiply(){
+   var result = 1;
+       for (let i = 0; i < arguments.length; i++){
+       result = result * arguments[i];
+   };
+   return result;
+  }
+}
+ let addition = new SimpleCalculator();
+ let multiplication = new SimpleCalculator();
 
-// console.log(add(1,2,3,4));
-// console.log(multiply(1,2,3,4));
+addition.add(1,2,3,4);
+multiplication.multiply(1,2,3,4);
